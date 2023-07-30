92304db1cbd59642118ccce180c8f6b67e123e4d
kim <gangssu0@gmail.com>
2023-07-31 00:14:20 +0900

    complete

---
+++ script/dan99.js
@@ -0,0 +1,36 @@
+let result1 = document.querySelector(".contents:nth-child(2) .result");
+let result2 = document.querySelector(".contents:nth-child(3) .result");
+let result3 = document.querySelector(".contents:nth-child(4) .result");
+let result4 = document.querySelector(".contents:nth-child(5) .result");
+let result5 = document.querySelector(".contents:nth-child(6) .result");
+let result6 = document.querySelector(".contents:nth-child(7) .result");
+let result7 = document.querySelector(".contents:nth-child(8) .result");
+let result8 = document.querySelector(".contents:nth-child(9) .result");
+let result9 = document.querySelector(".contents:nth-child(10) .result");
+const resultBtn = document.querySelector('#result_btn');
+const resetBtn = document.querySelector('#reset_btn');
+let dan2 = 2
+console.log(result1, result2, result3, result4, result5, result6, result7, result8, result9, resultBtn, resetBtn);
+
+resultBtn.addEventListener('click',function(){
+    result1.innerHTML = dan2*1;
+    result2.innerHTML = dan2*2;
+    result3.innerHTML = dan2*3;
+    result4.innerHTML = dan2*4;
+    result5.innerHTML = dan2*5;
+    result6.innerHTML = dan2*6;
+    result7.innerHTML = dan2*7;
+    result8.innerHTML = dan2*8;
+    result9.innerHTML = dan2*9;
+});
+resetBtn.addEventListener('click',function(){
+    result1.innerHTML = null;
+    result2.innerHTML = null;
+    result3.innerHTML = null;
+    result4.innerHTML = null;
+    result5.innerHTML = null;
+    result6.innerHTML = null;
+    result7.innerHTML = null;
+    result8.innerHTML = null;
+    result9.innerHTML = null;
+});
---
+++ script/scale.js
@@ -0,0 +1,20 @@
+let userHeight = document.querySelector('#user_height');
+let userWeight = document.querySelector('#user_weight');
+const resultBtn = document.querySelector('#result_btn');
+const resetBtn = document.querySelector('#reset_btn');
+let result = document.querySelector('.result');
+
+let nomal_w = 0;
+console.log(userHeight, userWeight, resultBtn, resetBtn, result)
+resultBtn.addEventListener('click',function(){
+    nomal_w = (userHeight.value-100)*0.9;
+    console.log(nomal_w);
+    result.style.display = 'block';
+    result.innerHTML = `적정 체중은 ${nomal_w}kg이며,  ${userWeight.value-nomal_w}kg 초과 되셨습니다.`
+});
+resetBtn.addEventListener('click',function(){
+    result.innerHTML = null;
+    userHeight.value = null;
+    userWeight.value = null;
+    result.style.display = 'none';
+});
